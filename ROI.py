111b10a3d3419261f0454b2c31ef819bbb946d09
UNILESS <sunup322@gmail.com>
2021-10-12 16:01:06 +0900

    10/12

---
+++ ROI.py
@@ -0,0 +1,31 @@
+import cv2
+
+# Load image, grayscale, Otsu's threshold
+image = cv2.imread('photos/256_nobg/24bit/KakaoTalk_20211007_224543118_04-removebg-preview.png')
+original = image.copy()
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+# Morph close
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1,1))
+close = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=1)
+
+cv2.imshow('',close)
+cv2.waitKey()
+
+# Find contours and extract ROI
+cnts = cv2.findContours(close, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+print(cnts)
+num = 0
+
+for c in cnts:
+    x,y,w,h = cv2.boundingRect(c)
+    cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 1)
+    ROI = original[y:y+h, x:x+w]
+    cv2.imwrite('ROI_{}.png'.format(num), ROI)
+    num += 1
+
+cv2.imshow('image', image)
+cv2.imwrite('ROI_INPUT.png',image)
+cv2.waitKey()
---
+++ morphological.py
@@ -0,0 +1,60 @@
+import cv2;
+import numpy as np;
+
+# Read image
+im_in = cv2.imread("photos/256_nobg/24bit/KakaoTalk_20211007_224543118_04-removebg-preview.png", cv2.IMREAD_GRAYSCALE);
+
+# Threshold.
+# Set values equal to or above 220 to 0.
+# Set values below 220 to 255.
+
+th, im_th = cv2.threshold(im_in, 150, 255, cv2.THRESH_BINARY)
+
+'''cv2.imshow("th", im_th)
+cv2.waitKey(0)
+cv2.destroyAllWindows()'''
+
+# Copy the thresholded image.
+im_floodfill = im_th.copy()
+
+# Mask used to flood filling.
+# Notice the size needs to be 2 pixels than the image.
+h, w = im_th.shape[:2]
+mask = np.zeros((h+2, w+2), np.uint8)
+
+# Floodfill from point (0, 0)
+cv2.floodFill(im_floodfill, mask, (0,0), 255)
+
+# Invert floodfilled image
+im_floodfill_inv = cv2.bitwise_not(im_floodfill)
+
+# Combine the two images to get the foreground.
+im_out = im_th | im_floodfill_inv
+
+'''cv2.imshow("", im_out)
+cv2.waitKey(0)
+cv2.destroyAllWindows()'''
+
+contours, hierarchy = cv2.findContours(im_out.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+final_contours = []
+for contour in contours:
+    area = cv2.contourArea(contour)
+    if area > 1:
+        final_contours.append(contour)
+
+
+counter = 0
+for c in final_contours:
+    counter = counter + 1
+# for c in [final_contours[0]]:
+    peri = cv2.arcLength(c, True)
+    approx = cv2.approxPolyDP(c, 0.000001 * peri, True)
+    x,y,w,h = cv2.boundingRect(approx)
+    print(x, y, w, h)
+    aspect_ratio = w / float(h)
+
+    if (aspect_ratio >= 0.8 and aspect_ratio <= 4):
+        cv2.rectangle(im_in,(x,y),(x+w,y+h),(255,0,0),1)
+        cv2.imwrite('splitted_{}.jpg'.format(counter), im_in[y:y+h, x:x+w])
+cv2.imwrite('rectangled_split.jpg', im_in)
