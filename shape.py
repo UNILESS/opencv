340ca030158411a9be72b83fd3589dd1430d1914
UNILESS <sunup322@gmail.com>
2021-11-01 14:51:18 +0900

    11/1

--- shape.py
+++ shape.py
@@ -1,6 +1,48 @@
 import math
 import cv2
 import numpy as np
+
+def crop_minAreaRect(img, rect):
+
+    # rotate img
+    angle = rect[2]
+    rows,cols = img.shape[0], img.shape[1]
+    M = cv2.getRotationMatrix2D((cols/2,rows/2),angle,1)
+    img_rot = cv2.warpAffine(img,M,(cols,rows))
+
+    # rotate bounding box
+    rect0 = (rect[0], rect[1], 0.0)
+    box = cv2.boxPoints(rect0)
+    pts = np.int0(cv2.transform(np.array([box]), M))[0]
+    pts[pts < 0] = 0
+
+    # crop
+    img_crop = img_rot[pts[1][1]:pts[0][1],
+                       pts[1][0]:pts[2][0]]
+
+    return img_crop
+
+
+def setLabel(img, pts, label):
+    (x, y, w, h) = cv2.boundingRect(pts)
+    pt1 = (x, y)
+    pt2 = (x + w, y + h)
+    print(pt1[0], pt1[1]-3)
+    cv2.rectangle(img, pt1, pt2, (0, 255, 0), 2)
+    cv2.putText(img, label, (pt1[0], pt1[1]-3), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255))
+
+
+def imsave(img, pts, num):
+    rect = cv2.minAreaRect(pts)
+    print(rect)
+    crop_min_rect = crop_minAreaRect(img, rect)
+    cv2.imwrite('SPL_{}.png'.format(num), crop_min_rect)
+
+    (x, y, w, h) = cv2.boundingRect(pts) # 현재 angle 값 못 넣고 있음.
+    pt1 = (x, y)
+    pt2 = (x + w, y + h)
+    cv2.rectangle(img, pt1, pt2, (0, 255, 0), 2)
+    ROI = original[y-3:y+h+3, x-3:x+w+3]
     cv2.imwrite('SPL_{}.png'.format(num), ROI)
 
 
@@ -21,7 +63,6 @@ for cont in contours:
     _, radius = cv2.minEnclosingCircle(cont)
 
     ratio = radius * radius * math.pi / area
-    print(vtc)
     if vtc >= 8:
         setLabel(img, cont, 'NonSpl ' + str(vtc))
     elif vtc < 8:
--- watershed.py
+++ watershed.py
@@ -1,29 +1,28 @@
-#-*-coding:utf-8-*-
+# -*-coding:utf-8-*-
 
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-
 # img = cv2.imread('images/watershed.jpg')
 img = cv2.imread('photos/256_nobg/24bit/KakaoTalk_20211007_224543118_04-removebg-preview.png')
 
 # binaray image로 변환
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 
-#Morphology의 opening, closing을 통해서 노이즈나 Hole제거
-kernel = np.ones((3,3),np.uint8)
-opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel,iterations=1)
+# Morphology의 opening, closing을 통해서 노이즈나 Hole제거
+kernel = np.ones((3, 3), np.uint8)
+opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=1)
 
 # dilate를 통해서 확실한 Backgroud
-sure_bg = cv2.dilate(opening,kernel,iterations=3)
+sure_bg = cv2.dilate(opening, kernel, iterations=3)
 
-#distance transform을 적용하면 중심으로 부터 Skeleton Image를 얻을 수 있음.
+# distance transform을 적용하면 중심으로 부터 Skeleton Image를 얻을 수 있음.
 # 즉, 중심으로 부터 점점 옅어져 가는 영상.
 # 그 결과에 thresh를 이용하여 확실한 FG를 파악
-dist_transform = cv2.distanceTransform(opening,cv2.DIST_L2,5)
-ret, sure_fg = cv2.threshold(dist_transform,0.5*dist_transform.max(),255,0)
+dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2, 5)
+ret, sure_fg = cv2.threshold(dist_transform, 0.5 * dist_transform.max(), 255, 0)
 sure_fg = np.uint8(sure_fg)
 
 # Background에서 Foregrand를 제외한 영역을 Unknow영역으로 파악
@@ -35,15 +34,14 @@ markers = markers + 1
 markers[unknown == 255] = 0
 
 # watershed를 적용하고 경계 영역에 색지정
-markers = cv2.watershed(img,markers)
-img[markers == -1] = [255,0,0]
-
+markers = cv2.watershed(img, markers)
+img[markers == -1] = [255, 0, 0]
 
 images = [gray, thresh, sure_bg, dist_transform, sure_fg, unknown, markers, img]
-titles = ['Gray','Binary','Sure BG','Distance','Sure FG','Unknown','Markers','Result']
+titles = ['Gray', 'Binary', 'Sure BG', 'Distance', 'Sure FG', 'Unknown', 'Markers', 'Result']
 
 for i in range(len(images)):
-    plt.subplot(2,4,i+1),plt.imshow(images[i]),plt.title(titles[i]),plt.xticks([]),plt.yticks([])
+    plt.subplot(2, 4, i + 1), plt.imshow(images[i]), plt.title(titles[i]), plt.xticks([]), plt.yticks([])
 
 cv2.imwrite('result_photo/result_watershed_4.png', img)
 
